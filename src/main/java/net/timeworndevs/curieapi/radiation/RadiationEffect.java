ab117c8050cbc8cdbf5f6abf3bd3e1e808358df0
ItsToastCraft <itstoastcraft@gmail.com>
2025-07-18 15:31:50 -0400

    Radiation effects now use radiation entries

--- src/main/java/net/timeworndevs/curieapi/radiation/RadiationEffect.java
+++ src/main/java/net/timeworndevs/curieapi/radiation/RadiationEffect.java
@@ -2,9 +2,7 @@ package net.timeworndevs.curieapi.radiation;
 
 import net.minecraft.server.network.ServerPlayerEntity;
 
-import java.util.Map;
-
 public interface RadiationEffect {
     // Applies the effect determined by the class.
-    void applyEffect(ServerPlayerEntity player, Map<RadiationType, Float> types);
+    void applyEffect(ServerPlayerEntity player, RadiationEntry types);
 }
